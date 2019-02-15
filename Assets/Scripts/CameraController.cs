5723c521430f0ab2aa5f26f864b5d001df4f6b78
Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>
2019-02-15 09:35:24 -0500

    initial

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    private void FixedUpdate()
+    {
+        transform.right = target.position - transform.position;
+        transform.LookAt(target);
+    }
+}
