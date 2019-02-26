ca34e584928d5ff98bf6d603ce57b78a0dfcf2f0
nick <@>
2019-02-26 10:10:29 -0500

    need to add movement ignoring rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,22 +15,28 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        float AngleRad = Mathf.Atan2(mouse.y - transform.position.y, mouse.x - transform.position.x);
+        float AngleDeg = (180 / Mathf.PI) * AngleRad;
+        this.transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
             transform.Translate(new Vector2(0, 2) * moveSpeed * Time.deltaTime);
         }
 
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
             transform.Translate(new Vector2(0, -2) * moveSpeed * Time.deltaTime);
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             transform.Translate(new Vector2(-2, 0) * moveSpeed * Time.deltaTime);
         }
 
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             transform.Translate(new Vector2(2, 0) * moveSpeed * Time.deltaTime);
         }
